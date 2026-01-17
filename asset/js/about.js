37728489ccf035a1e55a3edda7a29b1e77af5d7a
Abdullah23890 <abdullah23890@gmail.com>
2026-01-17 18:55:51 +0500

    about page complete

--- asset/js/about.js
+++ asset/js/about.js
@@ -17,3 +17,46 @@ return false;
 return false;
   }
 }
+
+const elements = document.querySelectorAll(".animate");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) return;
+
+      const dir = entry.target.dataset.animate;
+
+      let startTransform = "translateY(80px)";
+
+      if (dir === "left") startTransform = "translateX(-120px)";
+      if (dir === "right") startTransform = "translateX(120px)";
+      if (dir === "bottom") startTransform = "translateY(120px)";
+
+      entry.target.animate(
+        [
+          {
+            transform: startTransform,
+            opacity: 0
+          },
+          {
+            transform: "translate(0,0)",
+            opacity: 1
+          }
+        ],
+        {
+          duration: 1100, // smooth time
+          easing: "cubic-bezier(0.16, 1, 0.3, 1)", // ultra smooth
+          fill: "forwards"
+        }
+      );
+
+      observer.unobserve(entry.target);
+    });
+  },
+  {
+    threshold: 0.25
+  }
+);
+
+elements.forEach((el) => observer.observe(el));
